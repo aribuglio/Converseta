29c53d995e1ba8d5c992c0c53013d1281b8d3b66
BUGLIO ARIEL <buglioariel@gmail.com>
2023-02-07 11:50:49 -0300

    ensaje

--- src/Persona.java
+++ src/Persona.java
@@ -1,36 +1,42 @@
+
 import java.util.Random;
 import java.util.Scanner;
 
 
 public class Persona { 
 
+	//Atributos de persona
 	
 	private String nombre, apellido,domicilio;
+	
+	
 	private boolean simpatico;
+	
 	private String[] Humor = new String[] {"Bien","Mal"};
 	
-	private String[] Amigos = new String[5];
-	
+		
 	private Scanner scan = new Scanner(System.in);
 	private Random rand = new Random();
+		
 	private int H = rand.nextInt(2);
 	private int H2 = rand.nextInt(2);
-	private int NumDistancia= rand.nextInt(2);
+	private int Num = rand.nextInt(2);
+	private int NumDistancia;
+
 	
-	//constructor
+	
+	//constructores
 	
 	public  Persona (String nombre, String apellido) {
 		this.nombre = nombre;
-		this.apellido = apellido;
-		
-	}
-
-	
-	//Metodos
+		this.apellido = apellido;	
+	}	
 	
 	
 	
-	//HUMOR
+		//Metodos
+	
+	//metodo que da HUMOR
 	
 		public String DarHumorRandom (){
 	        return Humor[rand.nextInt(Humor.length)];
@@ -43,133 +49,190 @@ public class Persona {
 		public int HumorRandom2() {
 			return H2= rand.nextInt();
 		}
-
+		
+		public int SioNo() {
+			return Num = rand.nextInt();
+		}	
+	
+		//Metodo para SALUDOS
 	
 	
-		//SALUDOS
-	
-	public void DecirMiNombre () {
-		System.out.println(this.nombre + " " + this.apellido);
-	}
-	
+		
+	                          //Parametro de tipo persona que  se llama salamin.
 	public void SaludarPersona(Persona salamin) {
-		System.out.println("Ariel: Hola "+ salamin.getNombre()+ " soy el " + this.nombre + ", como estas ?");
+						// Nombre del objeto persona (this.nombre) + texto + paramentro.getNombre()	--> Primero hace referencia al nombre del objeto y luego al nombre del parametro que lo 
+						//ponemos luego en la instancia del objeto.
+		
+		System.out.println(this.nombre+": Hola "+ salamin.getNombre()+ " soy " + this.nombre + ", como estas ?");
 		
 	}
 	
-	public void ResponderSaludo(Persona persona) {
-		System.out.println("Mariano: Hola " + persona.getNombre() + ", " + "estoy "+ Humor[H] + " y vos?");
+	public void ResponderSaludo(Persona salamin) {
+		System.out.println(this.nombre+": Hola " + salamin.getNombre() + ", " + "estoy "+ Humor[H] + " y vos?");
 		
-	
+	}
+	public void ResponderSaludo2(Persona salamin) {
+		
 		if (H==1){
-			System.out.println("Ariel: Que mal amigo,yo estoy " + Humor[H2] );
+			System.out.println(salamin.getNombre()+": Que mal amigo,yo estoy " + Humor[H2] );
 			
 		}			
 		
 		else if(H==0) {
-			System.out.println("Ariel: Me alegro amigo, yo estoy " + Humor[H2]);
+			System.out.println(salamin.getNombre()+": Me alegro amigo, yo estoy " + Humor[H2]);
 			
 		}
 	}
+	
 		
-	public void ResponderSaludo2(Persona salamin) {
+	public void ResponderSaludo3(Persona salamin) {
 		
 		
 		if (H2==1){
-			System.out.println("Mariano: Que garron loco " );
+			System.out.println(this.nombre+": Que garron loco " );
 			
 		}			
 		
 		else if(H2==0) {
-			System.out.println("Mariano: Me alegro por vos");
+			System.out.println(this.nombre+": Me alegro por vos");
 			
 		}
 	}	
 			
 			
-		//INVITACION	
+		//Metodo INVITACION	
 	
 	public void InvitarFiesta (Persona salamin){
 		if(H2 == 1) {
-			System.out.println("Ariel: Te llamo para invitarte a una fiesta, pero si estas mal no creo que quieras, que decis? ");
+			System.out.println(this.nombre+": Te llamo para invitarte a una fiesta, pero si estas mal no creo que quieras, que decis? ");
 		}
 		else {
-			System.out.println("Ariel: Te llamo por que estoy organizando una fiesta, queres venir?");
+			System.out.println(this.nombre+": Te llamo por que estoy organizando una fiesta, queres venir?");
 		}	
 	}
 	
-	public void ResponderInvitacion (Persona persona) {
+	public void ResponderInvitacion (Persona salamin) {
 		if (simpatico == true) {
-			System.out.println("Mariano: Dale me re gustaria, en donde es?");
+			System.out.println(this.nombre+": Dale me re gustaria, en donde es?");
 			//responde que si
 		}
 		
 		else if(simpatico == false){
-			System.out.println("Mariano: No me pinta gracias bro");
+			System.out.println(this.nombre+": No me pinta gracias bro");
 			//Responde que no 
 		}
 	}
 	
-	//DOMICILIO
+	//Metodo decir DOMICILIO
 	
 	public void DecirMiDomicilio(Persona salamin) {
 		
 		if(this.domicilio != null) {
-			System.out.println("Ariel: En mi casa, la direccion es: " + this.domicilio);
+			System.out.println(this.nombre+": En mi casa, la direccion es: " + this.domicilio);
 		}
 		else {
-			System.out.println("Ariel: No se mi domicilio");
+			System.out.println(this.nombre+": No se mi domicilio");
 		}
 	}
 	
-	//Distancia
+	//Metodo Distancia
 	
-	public int Distancia() {
-		return NumDistancia= rand.nextInt();
+	public void Distancia(Persona salamin) {
+		System.out.println(this.nombre+": A cuantos KM queda de mi casa ?");
+		
+		
+		//Colocar por teclado la distancia 
+		System.out.println(salamin.getNombre()+": Queda a:");
+		NumDistancia = scan.nextInt();
+		
+		
 	}
 	
 	
 	//Respuesta a domicilio
 	
-	public void RespuestaDomicilio (Persona persona) {
+	public void RespuestaDomicilio (Persona salamin) {
 		
-		if(NumDistancia == 0 ) {
-			System.out.println("Mariano: Uuuuh me queda re lejos, me bajo hermano, gracias");
-			System.out.println("Ariel: Okay dale, no hay problema");
+		if(NumDistancia > 50 ) {
+			System.out.println(this.nombre+": Uuuuh me queda re lejos, me bajo hermano, gracias");
+			System.out.println(salamin.getNombre()+": Okay dale, no hay problema");
 			
 		}
 		
 		else {
-			System.out.println("Mariano: Genial me queda cerca, puedo invitar amigos? ");
-			System.out.println("Ariel: Sii ! Claro, hasta 5 podes invitar, pasame los nombres");
+			System.out.println(this.nombre+": Genial me queda cerca, puedo invitar amigos? ");
+			System.out.println(salamin.getNombre()+": Sii ! Claro, hasta 5 podes invitar, pasame los nombres:");
 		}
 		
 	}
 	
-	//Amigos
+	//Metodo decirAmigos
 	
-	
-	
-	public void DecirAmigos () {
-		if(NumDistancia==1) {
+	public void DecirAmigos (Persona salamin) {
+		if(NumDistancia < 50 ) {
 			
-			for(int i = 0; i < Amigos.length; i++) {
-				System.out.print("["+i+"]: ");
-				Amigos[i] = scan.next();
-					}
-				}		
-			else {
-				System.out.println("FIN");
-				
-			}
+			System.out.println(this.nombre+": Dale, los nombres de mis amigos son:");
+			System.out.println();
+		}
+		
 		
 	}
 	
 	
+	public void DecirAmigos2 (Persona salamin) {
+		
+		
+		if(NumDistancia < 50 ) {
+			System.out.println(nombre + apellido);
+			
+			
+		}		
+		
+	}
+		
+	//Metodo despedida 
+	
+	public void Despedida (Persona salamin) {
+		
+		if(NumDistancia > 50 ) {
+			System.out.println(this.nombre+": Dale gracias por invitar, nos vemos");
+			System.out.println(salamin.getNombre()+": De nada amigo, Nos vemos");
+		}
+		
+	}
+	
+	
+	public void RespuestaDeAmigos (Persona salamin) {
+		System.out.println(this.nombre + ": Perfecto, ahora los anoto");
+		System.out.println(this.nombre + ": Listo ! ");
+		
+	}
+	
+	public void OfrecerFavor (Persona salamin) {
+		System.out.println(this.nombre + ": Gracias "+ salamin.getNombre() + ", necesitas que compre algo?");
+		
+		
+	}
+	
+	public void RespuestaFavor	(Persona salamin) {
+		
+		if (Num == 1 ) {
+			System.out.println(this.nombre+": Si! Me re sirve si compras algo de alcohol");
+		}
+		
+		else {
+			System.out.println(this.nombre+": No gracias amigo, tenemos todo.");
+		}
+	}
+
 	
 	
 	//Get and set
 	
+	
+
+
+
 	public String getNombre() {
 		return nombre;
 	}
@@ -224,39 +287,45 @@ public class Persona {
 	public void setRand(Random rand) {
 		this.rand = rand;
 	}
-	
-		
-		
-		
-	
-	
-	
-			
-		
-	
-	
-	
-		
-		
-			
-	
-			
-		
-			
-		
-		
-	
-			
-	
-	
-	
+
+
+
+
 
 	
-	
-	
-	
-
 }
+	
+		
+		
+
+	
+	
+			
+		
+	
+	
+	
+		
+		
+			
+	
+			
+		
+			
+		
+		
+	
+			
+	
+	
+	
+
+	
+	
+	
+	
+
+
 	
 	
 	
--- src/Principal.java
+++ src/Principal.java
@@ -2,41 +2,90 @@
 public class Principal {
 
 	public static void main (String[]args) {
+	
+	//Array de tipo persona 
+
+		Persona [] amigos = new Persona[5];
 		
+	//Intanciamos los objetos
+		
+		
+	//nombre de la clase + Nombre del objeto = new + constructores	
+	
 		Persona Ariel = new Persona ( "Ariel","Buglio");
 		Persona Mariano = new Persona ("Mariano", "Buglio");
+		Persona Mili = 	new Persona ("Mili", "Vazquez");
+		
+	//Amigos	
+		
+		amigos[0]  = 	new Persona ("Agus", " Zabala");
+		amigos[1]  = 	new Persona ("Fede", " Pereira");
+		amigos[2]  = 	new Persona ("Flor", " Calzeta");
+		amigos[3]  = 	new Persona ("Gera", " Puchi");
+		amigos[4]  = 	new Persona ("Leonel", " Messi");
 		
 		
-		//Saludos
+	//Saludos
+		
+		//Dar accion a los metodos creados en la clase Persona 
+		//Objeto + metodo + (Argumento)
+		
 		Ariel.SaludarPersona(Mariano);
 		Mariano.ResponderSaludo(Ariel);
 		Mariano.ResponderSaludo2(Ariel);
+		Mariano.ResponderSaludo3(Ariel);
 		
 		System.out.println();
 		
-		//Invitacion
+	//Invitacion
+		
 		Ariel.InvitarFiesta(Mariano);
 		Mariano.setSimpatico(true);
 		Mariano.ResponderInvitacion(Ariel);
 		
-		//Domicilio
+	//Domicilio
 		
 		System.out.println();
 		
 		Ariel.setDomicilio("guardia vieja 2360");
 		Ariel.DecirMiDomicilio(Mariano);
 		
-		//Respuesta domicilio
+	//Respuesta domicilio
 		
+		Mariano.Distancia(Ariel);
 		Mariano.RespuestaDomicilio(Ariel);
+		
 
-		//Amigos
-		Mariano.DecirAmigos();
+	//DecirAmigos
+		
+		Mariano.DecirAmigos(Ariel);
+		for(int i = 0; i<5; i++) {
+			amigos[i].DecirAmigos2(Mariano);
+		}
+		
+	//Despedida
+		
+		Mariano.Despedida(Ariel);
 		
 		
+	//Respuesta Amigos 
 		
+		System.out.println();
+		Ariel.RespuestaDeAmigos(Mariano);
 		
+	//OfrecerFavor
+		
+		Mariano.OfrecerFavor(Ariel);
+	
+	//RespuestaFavor	
+		
+		Ariel.RespuestaFavor(Mariano);
 		
 	}
+	
+		
+	
+		
+		
 	
 }
